340eb8207e395a20a0c5109d5fc01c43103920bd
Jonathan Cho <jon88cho@gmail.com>
2018-03-10 21:43:03 -0500

    Final Draft, added Bayes theorem and Hypothesis Testing, fixed up the formatting

--- Defense/Defensive_Stats.py
+++ Defense/Defensive_Stats.py
@@ -19,9 +19,19 @@ def player_stats():
         temp_df['Year'] = Series ([i-1 for j in range(len(temp_df.index))])
         Player_info = Player_info.append(temp_df)
     return Player_info
-#Will return 4 dataframes
-def Defensive_Stats():
+def steals_df():
+    dframe_list = pd.io.html.read_html("https://www.basketball-reference.com/leaders/stl_yearly.html")
+    Steals_df = dframe_list[0]
+    Steals_df = Steals_df.drop(index =0)
+    Steals_df = Steals_df.drop(columns =[1])
+    Steals_df = Steals_df.rename(columns = {0:"Year",2:"Player",3:"Steals",4:"Team"})
+    #Steals_df = Steals_df.drop(index = 0)
+    Steals_df['Year'] = Steals_df['Year'].astype(str).apply(lambda x: x[0:4])
+    Steals_df['Year'] = Steals_df['Year'].astype(float)
+    return Steals_df
+
     #Defense Tracking
+def Defensive_Tracking():
     Defensive_Tracking = DataFrame()
     for i in range (13,18):
         df = pd.read_csv("Player_Csvs/Defense_Tracking/Defense%d.csv" % (i))
@@ -31,7 +41,9 @@ def Defensive_Stats():
     Defensive_Tracking = Defensive_Tracking.rename(columns = {'TEAM_ABBREVIATION':'Team','PLAYER_NAME': 'Player'})
     Defensive_Tracking['Player'] = Defensive_Tracking['Player'].apply(cleanup)
     Defensive_Tracking.loc[:,'GP':'Year']= Defensive_Tracking.loc[:,'GP':'Year'].astype(float)
+    return Defensive_Tracking
     #Defense_General
+def Defense_General():
     Defense_General = DataFrame()
     for i in range (96,100):
         df1 = pd.read_csv("Player_Csvs/Defensive_General/Defense%d.csv" % (i))
@@ -50,7 +62,9 @@ def Defensive_Stats():
     Defense_General.loc[:,'Age':'DEF_WS_RANK']= Defense_General.loc[:,'Age':'DEF_WS_RANK'].replace([' None'], '0')
     Defense_General.loc[:,'Age':'DEF_WS_RANK']= Defense_General.loc[:,'Age':'DEF_WS_RANK'].astype(float)
     Defense_General = Defense_General.drop_duplicates()
+    return Defense_General
     #Shooting
+def Shooting():
     Shooting = DataFrame()
     for i in range (96,100):
         df1 = pd.read_csv("Player_Csvs/Opponent_Shooting/Shooting%d.csv" % (i))
@@ -78,8 +92,9 @@ def Defensive_Stats():
                    ' OPP_FGA.7',' OPP_FG_PCT.7',' OPP_FGM.8',' OPP_FG_PCT.8','Age','Team'],axis=1)
     Shooting.loc[:,'<5Ft FGM':'Year'] = Shooting.loc[:,'<5Ft FGM':'Year'].replace([' None'], '0')
     Shooting.loc[:,'<5Ft FGM':'Year']= Shooting.loc[:,'<5Ft FGM':'Year'].astype(float)
-
+    return Shooting
     #Hustle Stats
+def Hustle():
     Hustle = DataFrame()
     for i in range (15,18):
         df = pd.read_csv("Player_Csvs/Hustle/Hustle%d.csv" % (i))
@@ -89,4 +104,4 @@ def Defensive_Stats():
     Hustle = Hustle.rename(columns = {'PLAYER_NAME':"Player","TEAM_ABBREVIATION":"Team"})
     Hustle['Player'] = Hustle['Player'].astype(str).apply(cleanup)
     
-    return Defensive_Tracking,Defense_General,Shooting,Hustle
+    return Hustle
